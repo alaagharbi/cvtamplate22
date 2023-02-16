7691fe4d8573bd660adc908f5d2b19e2fffe7714
alaagharbi <52264121+alaagharbi@users.noreply.github.com>
2023-02-16 23:52:44 +0100

    Initial commit

---
+++ app/src/main/java/com/example/cvtamplate2/utils/Converters.kt
@@ -0,0 +1,16 @@
+package com.example.cvtamplate2.utils
+
+import androidx.room.TypeConverter
+import java.sql.Date
+
+class Converters {
+    @TypeConverter
+    fun fromTimestamp(value: String?): Date? {
+        return value?.let { Date.valueOf(it) }
+    }
+
+    @TypeConverter
+    fun dateToTimestamp(date: Date?): String? {
+        return date?.toString()
+    }
+}
